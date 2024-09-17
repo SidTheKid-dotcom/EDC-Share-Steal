4c5e080d776fa124578098c56b4a6dba871d6e96
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-09-18 01:16:03 +0530

    Changed Redis Name for Connection

--- services/rateLimiting.js
+++ services/rateLimiting.js
@@ -1,6 +1,6 @@
 const Redis = require('ioredis');
 const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
+  host: process.env.REDIS_HOST || 'redis',
   port: process.env.REDIS_PORT || 6379,
 });
 
