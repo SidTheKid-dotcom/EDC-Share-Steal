5ee4314f5d4bce5e4699f58393b208447d87e041
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-09-17 18:23:48 +0530

    Added CICD Pipeline with Docker Containers

--- services/rateLimiting.js
+++ services/rateLimiting.js
@@ -1,5 +1,8 @@
 const Redis = require('ioredis');
-const redis = new Redis();
+const redis = new Redis({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+});
 
 const connectionLimit = 30;
 const connectionWindowMs = 10 * 60 * 1000; // 10 minutes
