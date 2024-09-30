c789f46590f218c12171ede9bcd4f4af84ece01c
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-09-30 15:15:09 +0530

    DEV Route Allowed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 // Configure CORS to allow only your anywhere
 app.use(
   cors({
-    origin: "http://edc-steal-share.s3-website.ap-south-1.amazonaws.com/", // Allow requests from any origin
+    origin: ["http://edc-steal-share.s3-website.ap-south-1.amazonaws.com/", "http://localhost:5173/"], // Allow requests from any origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Specify allowed methods
     allowedHeaders: "Content-Type,Authorization", // Specify allowed headers
     credentials: true, // Allow cookies and authentication headers
