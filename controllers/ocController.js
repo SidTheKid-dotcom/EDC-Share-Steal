7723f138f6c4541cf89971a8587607dfd429aecc
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-10-01 11:57:22 +0530

    Fixed OC Prisma

--- controllers/ocController.js
+++ controllers/ocController.js
@@ -1,3 +1,6 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 exports.getAllPlayers = async (req, res) => {
     try {
         const players = await prisma.user.findMany({
